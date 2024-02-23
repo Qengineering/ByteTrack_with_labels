1629042b317c5bdecc404216ad4c926fec714b5c
Q-engineering <info@qengineering.eu>
2024-02-23 13:39:35 +0100

    Update utils.cpp

--- BYTETrack/src/utils.cpp
+++ BYTETrack/src/utils.cpp
@@ -156,6 +156,33 @@ void BYTETracker::linear_assignment(vector<vector<float> > &cost_matrix, int cos
 	}
 }
 
+float BYTETracker::IoU(Ttlwh &box1, Ttlwh &box2)
+{
+	float minx1 = box1.l;
+	float maxx1 = box1.l + box1.w;
+	float miny1 = box1.t;
+	float maxy1 = box1.t+ box1.h;
+
+	float minx2 = box2.l;
+	float maxx2 = box2.l + box2.w;
+	float miny2 = box2.t;
+	float maxy2 = box2.t + box2.h;
+
+	if (minx1 > maxx2 || maxx1 < minx2 || miny1 > maxy2 || maxy1 < miny2)
+		return 0.0f;
+	else
+	{
+		float dx = std::min(maxx2, maxx1) - std::max(minx2, minx1);
+		float dy = std::min(maxy2, maxy1) - std::max(miny2, miny1);
+		float area1 = (maxx1 - minx1)*(maxy1 - miny1);
+		float area2 = (maxx2 - minx2)*(maxy2 - miny2);
+		float inter = dx*dy; // Intersection
+		float uni = area1 + area2 - inter; // Union
+		float IoU = inter / uni;
+		return IoU;
+	}
+}
+
 vector<vector<float> > BYTETracker::ious(vector<Ttlbr> &atlbrs, vector<Ttlbr> &btlbrs)
 {
 	vector<vector<float> > ious;
